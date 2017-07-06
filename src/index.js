0d12742aec4f0c1fc7a656402203c04d5fa7b168
Frankie Bagnardi <f.bagnardi@gmail.com>
2017-07-06 10:35:38 -0700

    readme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import TabsPage from './tabs';
+// import App from './tabs';
 // import App from './mui-button';
 //import App from './dropdown';
 import App from './time-input';
